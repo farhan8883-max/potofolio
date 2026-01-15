b9dd5c5cdc338970643631bff488eed8af0bd05f
farhan <177544242+farhan8883-max@users.noreply.github.com>
2026-01-16 06:18:34 +0700

    update

--- portofolio.js
+++ portofolio.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
-    strings : ["Designer","Superman","Developer"],
+    strings : ["Designer","Super","Dveloper"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
